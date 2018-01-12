1ea9fba3e8dc5b59a168874b5991d9d71bec1003
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-01-12 11:47:07 -0500

    Initial commit

---
+++ blast/build-image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USERNAME=slottad
+IMAGE=blast
+VERSION=`cat VERSION`
+
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-${VERSION}+-src.tar.gz
+
+docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
+docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
---
+++ contamfilter/build-image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+mkdir -p binaries
+cd binaries
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/blast/demo/vecscreen
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/VSlistTo1HitPerLine.awk
+
+mkdir -p ../databases
+cd ../databases
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/contam_in_euks.fa.gz
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/contam_in_prok.fa
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/adaptors_for_screening_euks.fa
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/adaptors_for_screening_proks.fa
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/mito.nt.gz
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/rrna.gz
+wget -nc ftp://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec
+cd ..
+
+USERNAME=slottad
+IMAGE=contamfilter
+VERSION=`cat VERSION`
+
+docker build -t $USERNAME/$IMAGE:$VERSION .
+docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
+
+
+#docker build -t slottad/contamfilter1809 .
+
