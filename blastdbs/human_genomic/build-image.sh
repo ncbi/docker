c724081d04ee45b0db945b5939cb99ffb630b9ca
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-01-30 11:33:52 -0500

    Removed extraneous databases directory.

--- blastdbs/human_genomic/build-image.sh
+++ blastdbs/human_genomic/build-image.sh
@@ -2,7 +2,6 @@
 #set -ex
 
 mkdir -p tarballs
-mkdir -p databases
 cd tarballs
 files=`curl -s -l ftp://ftp.ncbi.nlm.nih.gov/blast/db/ | grep ^human_genomic\..*\.tar\.gz$`
 for i in ${files}; do
@@ -16,5 +15,3 @@ VERSION=`cat VERSION`
 
 docker build -t $USERNAME/$IMAGE:$VERSION .
 docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
-
-# && tar xvf human_genomic.${i}.tar.gz -C ../databases --keep-newer-files
