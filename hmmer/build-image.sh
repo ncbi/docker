5688382b9455a0255e06c61e590ef8e78738c7b1
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-02-06 13:04:00 -0500

    Updated to Ubuntu:17.10 base docker image.

--- hmmer/build-image.sh
+++ hmmer/build-image.sh
@@ -4,7 +4,5 @@ USERNAME=ncbi
 IMAGE=hmmer
 VERSION=`cat VERSION`
 
-#wget -nc http://eddylab.org/software/hmmer3/${VERSION}/hmmer-${VERSION}.tar.gz
-
 docker build --build-arg VERSION=${VERSION} -t $USERNAME/$IMAGE:$VERSION . \
     && docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
