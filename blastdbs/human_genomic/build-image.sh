271cf10248720dbd36a891c1553e7999515b8452
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-02-11 20:41:10 -0500

    Move file procurement to Dockerfile

--- blastdbs/human_genomic/build-image.sh
+++ blastdbs/human_genomic/build-image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #set -ex
 
-mkdir -p tarballs
-cd tarballs
-files=`curl -s -l ftp://ftp.ncbi.nlm.nih.gov/blast/db/ | grep ^human_genomic\..*\.tar\.gz$`
-for i in ${files}; do
-    wget -nc ftp://ftp.ncbi.nlm.nih.gov/blast/db/${i}
-done
-cd ..
+# mkdir -p tarballs
+# cd tarballs
+# files=`curl -s -l ftp://ftp.ncbi.nlm.nih.gov/blast/db/ | grep ^human_genomic\..*\.tar\.gz$`
+# for i in ${files}; do
+#     wget -nc ftp://ftp.ncbi.nlm.nih.gov/blast/db/${i}
+# done
+# cd ..
 
 USERNAME=ncbi
 IMAGE=blast_human_genomic
