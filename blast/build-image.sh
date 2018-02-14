6c2a8d529ff9fa7c02b16653b7d5ae4980b787cf
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-02-14 18:55:04 +0000

    Moved src file download to dockerfile

--- blast/build-image.sh
+++ blast/build-image.sh
@@ -4,7 +4,7 @@ USERNAME=ncbi
 IMAGE=blast
 VERSION=`cat VERSION`
 
-wget -nc ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-${VERSION}+-src.tar.gz
+#wget -nc ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-${VERSION}+-src.tar.gz
 
 docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
 docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
