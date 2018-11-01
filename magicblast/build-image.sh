dd3ce7f56bc1a681aacd1c94b8b1adac28ed823f
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2018-11-01 16:52:55 -0400

    Magicblast in progress

---
+++ magicblast/build-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+USERNAME=christiam
+IMAGE=magicblast
+VERSION=`cat VERSION`
+
+docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
+docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
