9eb135e414fa3fc5ffbffca41f364b6ee0ca6f53
Christiam Camacho <christiam@hotmail.com>
2018-11-03 10:13:59 -0400

    + README

---
+++ blast-workbench/README.md
@@ -0,0 +1,10 @@
+# blast-workbench
+
+This docker image contains the latest versions of the following NCBI software:
+
+* [BLAST+](https://www.ncbi.nlm.nih.gov/books/NBK279690/) command line applications
+* [MagicBLAST](https://ncbi.github.io/magicblast/)
+* [EDirect](https://dataguide.nlm.nih.gov/edirect/documentation.html)
+
+
+The [test.sh](./test.sh) script shows some example commands.
--- blast-workbench/test.sh
+++ blast-workbench/test.sh
@@ -4,16 +4,12 @@
 # Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
 # Created: Sat Nov  3 08:44:39 2018
 
-time docker run --rm christiam/blast-workbench /bin/bash -c "printenv BLASTDB"
-time docker run --rm christiam/blast-workbench /bin/bash -c "ls -Srhl /blast/bin /blast/lib"
-time docker run --rm christiam/blast-workbench /bin/bash -c "ls -Srhl /magicblast/bin /magicblast/lib"
-#time docker run --rm christiam/blast-workbench /bin/bash -c "ldd /magicblast/bin/magicblast"
-time docker run --rm christiam/blast-workbench /bin/bash -c "ldd /blast/bin/blastdbcmd"
-time docker run --rm christiam/blast-workbench blastn -version
-time docker run --rm -w /blast/lib christiam/blast-workbench blastn -version
-time docker run --rm christiam/blast-workbench magicblast -version
-time docker run --rm -w /magicblast/lib christiam/blast-workbench magicblast -version
-time docker run --rm christiam/blast-workbench installconfirm
-time docker run --rm christiam/blast-workbench /bin/sh -c " esearch -db nucleotide -query u00001 | efetch -format fasta"
-time docker run --rm christiam/blast-workbench get_species_taxids.sh -n human
-time docker run --rm christiam/blast-workbench update_blastdb.pl --passive --decompress taxdb
+IMG=christiam/blast-workbench
+
+time docker run --rm ${IMG} /bin/bash -c "printenv BLASTDB"
+time docker run --rm ${IMG} blastn -version
+time docker run --rm ${IMG} magicblast -version
+time docker run --rm ${IMG} installconfirm
+time docker run --rm ${IMG} /bin/sh -c " esearch -db nucleotide -query u00001 | efetch -format fasta"
+time docker run --rm ${IMG} get_species_taxids.sh -n squirrel
+time docker run --rm ${IMG} update_blastdb.pl --passive --decompress taxdb
