cb9c1e1679915ab016efced4bf8d8305067586af
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-10-24 12:05:58 -0400

    Fix to previous commit

--- blast/2.10.0/build-image.sh
+++ blast/2.10.0/build-image.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 
 DOCKER_USERNAME=${1:-"ncbi"}
 IMAGE=blast
 VERSION=`cat VERSION`
 NP=`grep -c ^proc /proc/cpuinfo`
 
-docker build --build-arg version=${VERSION} --build-arg num_procs=${NP} -t $USERNAME/$IMAGE:$VERSION .
-docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
+docker build --build-arg version=${VERSION} --build-arg num_procs=${NP} -t $DOCKER_USERNAME/$IMAGE:$VERSION .
+docker tag $DOCKER_USERNAME/$IMAGE:$VERSION $DOCKER_USERNAME/$IMAGE:latest
