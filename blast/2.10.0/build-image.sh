5f15469117faddcfd215680ba29e90f32ded0528
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-10-24 12:37:57 -0400

    Rename variable for clarity

--- blast/2.10.0/build-image.sh
+++ blast/2.10.0/build-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eu
 
-DOCKER_USERNAME=${1:-"ncbi"}
+DOCKERHUB_USERNAME=${1:-"ncbi"}
 IMAGE=blast
 VERSION=`cat VERSION`
 NP=`grep -c ^proc /proc/cpuinfo`
 
-docker build --build-arg version=${VERSION} --build-arg num_procs=${NP} -t $DOCKER_USERNAME/$IMAGE:$VERSION .
-docker tag $DOCKER_USERNAME/$IMAGE:$VERSION $DOCKER_USERNAME/$IMAGE:latest
+docker build --build-arg version=${VERSION} --build-arg num_procs=${NP} -t $DOCKERHUB_USERNAME/$IMAGE:$VERSION .
+docker tag $DOCKERHUB_USERNAME/$IMAGE:$VERSION $DOCKERHUB_USERNAME/$IMAGE:latest
