29ee0ce969570dad94bb282e8659ebbe37e8ea40
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-11-15 20:34:46 +0000

    Bump edirect version, allow configurable image

--- edirect/test.sh
+++ edirect/test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --rm -it christiam/edirect installconfirm
+# test.sh: Perform some sanity checks on the edirect docker image
+#
+# Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
+# Created: Fri 07 Dec 2018 03:45:23 PM EST
+IMG=${1:-"ncbi/edirect:latest"}
+docker run --rm -it ${IMG} installconfirm
+docker run --rm -it ${IMG} efetch -db nucleotide -id u00001 -format fasta
