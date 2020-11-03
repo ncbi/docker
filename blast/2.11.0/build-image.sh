07b19a12d7d14bbfb6333e00ffc8c9877b5b374d
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2020-11-03 16:00:53 -0500

    Use VERSION file on FTP site

--- blast/2.11.0/build-image.sh
+++ blast/2.11.0/build-image.sh
@@ -2,7 +2,7 @@
 
 DOCKERHUB_USERNAME=${1:-"ncbi"}
 IMAGE=blast
-VERSION=`cat VERSION`
+VERSION=`curl -s https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/VERSION`
 NP=`grep -c ^proc /proc/cpuinfo`
 
 docker build --build-arg version=${VERSION} --build-arg num_procs=${NP} -t $DOCKERHUB_USERNAME/$IMAGE:$VERSION .
