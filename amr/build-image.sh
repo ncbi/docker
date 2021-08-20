0bf9c80fbe768d0f79112b5bbcac20ccfaa37119
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2021-08-20 14:41:16 -0400

    Update for release 3.10.14

--- amr/build-image.sh
+++ amr/build-image.sh
@@ -2,7 +2,7 @@
 
 USERNAME=ncbi
 IMAGE=amr
-VERSION=1.5
+VERSION=1.6
 DB_VERSION=`curl https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/latest/version.txt`
 
 docker build --build-arg VERSION=${VERSION} --build-arg DB_VERSION=${DB_VERSION} -t $USERNAME/$IMAGE:$DB_VERSION . \
