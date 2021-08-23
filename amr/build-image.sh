769e661c5a845f5abc58d5b3ddc0a86f58fc3e0b
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2021-08-23 15:12:39 -0400

    Updates to improve tagging and automate rebuilds
    build-image.sh should now read database version from
        https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/latest/version.txt
    and software version from:
        https://raw.githubusercontent.com/ncbi/amr/master/version.txt
    To rebuild those sections of the Dockerfile automatically if either of
    them changes.

--- amr/build-image.sh
+++ amr/build-image.sh
@@ -3,8 +3,12 @@
 USERNAME=ncbi
 IMAGE=amr
 VERSION=1.6
-DB_VERSION=`curl https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/latest/version.txt`
+DB_VERSION=`curl --silent https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/latest/version.txt`
+SOFTWARE_VERSION=`curl --silent https://raw.githubusercontent.com/ncbi/amr/master/version.txt`
+VERSION_TAG="${SOFTWARE_VERSION}-$DB_VERSION"
 
-docker build --build-arg VERSION=${VERSION} --build-arg DB_VERSION=${DB_VERSION} -t $USERNAME/$IMAGE:$DB_VERSION . \
-    && docker tag $USERNAME/$IMAGE:$DB_VERSION $USERNAME/$IMAGE:latest
+docker build --build-arg VERSION=${VERSION} --build-arg DB_VERSION=${DB_VERSION} \
+    --build-arg SOFTWARE_VERSION=${SOFTWARE_VERSION} \
+    -t $USERNAME/$IMAGE:$VERSION_TAG . \
+    && docker tag $USERNAME/$IMAGE:$VERSION_TAG $USERNAME/$IMAGE:latest
 
