37ac6eb39f7f5d7771fed1cba2d47370d8ddcdca
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2021-08-23 15:31:14 -0400

    Add a comment to clarify what $VERSION is for

--- amr/build-image.sh
+++ amr/build-image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Increment this to build from scratch including dependencies
+VERSION=1.6
+
 USERNAME=ncbi
 IMAGE=amr
-VERSION=1.6
 DB_VERSION=`curl --silent https://ftp.ncbi.nlm.nih.gov/pathogen/Antimicrobial_resistance/AMRFinderPlus/database/latest/version.txt`
 SOFTWARE_VERSION=`curl --silent https://raw.githubusercontent.com/ncbi/amr/master/version.txt`
 VERSION_TAG="${SOFTWARE_VERSION}-$DB_VERSION"
