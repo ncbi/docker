d7c814c2a7782f48a1316047271b1580f3365be4
boratyng <boratyng@ncbi.nlm.nih.gov>
2022-04-13 19:39:52 +0000

    Added BLAST+ static ARM image

--- blast-static/build-image.sh
+++ blast-static/build-image.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-USERNAME=slottad
+USERNAME=${1:-"ncbi"}
 IMAGE=blast-static
 VERSION=`cat VERSION`
 
-docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
+case $(uname -m) in
+	x86_64) ARCH=x64;;
+	aarch64) ARCH=x64-arm;;
+esac
+
+docker build --build-arg version=${VERSION} --build-arg arch=${ARCH} -t $USERNAME/$IMAGE:$VERSION .
 docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
