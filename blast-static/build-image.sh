e23abc86536d1114853493e66eb1589fd2a428d6
boratyng <boratyng@ncbi.nlm.nih.gov>
2022-04-14 20:23:03 +0000

    Additional tag for arm images

--- blast-static/build-image.sh
+++ blast-static/build-image.sh
@@ -4,5 +4,10 @@ USERNAME=${1:-"ncbi"}
 IMAGE=blast-static
 VERSION=`cat VERSION`
 
-docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
+ARCH=
+if [ $(uname -m) != x86_64 ] ; then
+    ARCH="-$(uname -m)"
+fi
+
+docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:${VERSION}${ARCH} .
 docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
