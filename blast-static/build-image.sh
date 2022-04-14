9526dd942c40dd9c45f7b8f7fdc6628e81409701
EC2 Default User <ec2-user@ip-172-16-120-202.ec2.internal>
2022-04-14 20:15:28 +0000

    Auto detect architecture

--- blast-static/build-image.sh
+++ blast-static/build-image.sh
@@ -4,10 +4,5 @@ USERNAME=${1:-"ncbi"}
 IMAGE=blast-static
 VERSION=`cat VERSION`
 
-case $(uname -m) in
-	x86_64) ARCH=x64;;
-	aarch64) ARCH=x64-arm;;
-esac
-
-docker build --build-arg version=${VERSION} --build-arg arch=${ARCH} -t $USERNAME/$IMAGE:$VERSION .
+docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
 docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
