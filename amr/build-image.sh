3d0209425910ac7d4f7ad89e17704716dd4a91e0
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2022-06-02 13:48:11 -0400

    Force rebuild to pick up new test_amrfinder.sh
    This caused a build failure because tests failed on changes in
    test_mut_all.expected. I don't think many people use this so I'm
    removing that test from the distribution.

--- amr/build-image.sh
+++ amr/build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Increment this to build from scratch including dependencies
-VERSION=1.7
+VERSION=1.8
 
 get_tarball_url() {
     curl --silent "https://api.github.com/repos/$1/releases/latest" |
