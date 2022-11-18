e454f8aa9476e5c2e792a58553ab05be9ff371b4
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2022-11-18 13:31:43 -0500

    Increment version to force rebuild

--- amr/build-image.sh
+++ amr/build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Increment this to build from base including dependencies
-VERSION=1.9
+VERSION=1.10
 
 get_tarball_url() {
     curl --silent "https://api.github.com/repos/$1/releases/latest" |
