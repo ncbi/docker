82838f9cb38552bcacdcc568082160ae6a22f604
Greg Boratyn <boratyng@ncbi.nlm.nih.gov>
2023-05-25 16:49:59 -0400

    Adding Jians files for igblast docker image

---
+++ igblast/build-image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+USERNAME=ncbi
+IMAGE=IgBLAST
+VERSION=`cat VERSION`
+NP=`grep -c proc /proc/cpuinfo`
+
+docker build --build-arg num_procs=${NP} --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
+docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
