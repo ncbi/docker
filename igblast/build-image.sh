07071a87b8ad51e3215fdcde7316e8dca148323f
boratyng <boratyng@ncbi.nlm.nih.gov>
2023-05-25 21:21:46 +0000

    Image name must be lowercase, removed unnecessary NP env variable

--- igblast/build-image.sh
+++ igblast/build-image.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -xe
 
 USERNAME=ncbi
-IMAGE=IgBLAST
+IMAGE=igblast
 VERSION=`cat VERSION`
-NP=`grep -c proc /proc/cpuinfo`
 
-docker build --build-arg num_procs=${NP} --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
+docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
 docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
