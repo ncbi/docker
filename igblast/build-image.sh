ed6a171bb203bca9d348c10ea551c789da15576a
boratyng <boratyng@ncbi.nlm.nih.gov>
2023-05-26 20:29:35 +0000

    Replaced build-image.sh script with Makefile

--- igblast/build-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-USERNAME=ncbi
-IMAGE=igblast
-VERSION=`cat VERSION`
-
-docker build --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
-docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
