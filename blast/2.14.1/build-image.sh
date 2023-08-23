e1b14a7014d6ec5c68843424460dac5f04c06abc
Yury Merezhuk <merezhuk@ncbi.nlm.nih.gov>
2023-08-23 13:35:42 -0400

    The 2.14.1 release

---
+++ blast/2.14.1/build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+
+DOCKERHUB_USERNAME=${1:-"ncbi"}
+IMAGE=blast
+VERSION=$(cat VERSION)
+# Check the latest ncbi-vdb release version in https://github.com/ncbi/ncbi-vdb/tags
+# To see which VDB version is currently used by C++ Toolkit do:
+#  grep local_vdb_base src/build-system/config.site.ncbi
+VDB_VERSION=3.0.6
+NP=`grep -c ^proc /proc/cpuinfo`
+
+docker build --build-arg version=${VERSION} --build-arg num_procs=${NP} --build-arg vdb_version=${VDB_VERSION} -t $DOCKERHUB_USERNAME/$IMAGE:$VERSION .
+docker tag $DOCKERHUB_USERNAME/$IMAGE:$VERSION $DOCKERHUB_USERNAME/$IMAGE:latest
