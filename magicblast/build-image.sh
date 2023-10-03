ce09a600a703692d5ed0a4743ff7b239b26a0b1c
Greg Boratyn <boratyng@ncbi.nlm.nih.gov>
2023-10-03 10:47:47 -0400

    * Build Magic-BLAST 1.7.2 * Include makeblastdb * Use a stable ncbi-vdb version

--- magicblast/build-image.sh
+++ magicblast/build-image.sh
@@ -3,7 +3,11 @@
 USERNAME=ncbi
 IMAGE=magicblast
 VERSION=`cat VERSION`
+# Check the latest ncbi-vdb release version in https://github.com/ncbi/ncbi-vdb/tags
+# To see which VDB version is currently used by C++ Toolkit do:
+#  grep local_vdb_base src/build-system/config.site.ncbi
+VDB_VERSION=3.0.7
 NP=`grep -c proc /proc/cpuinfo`
 
-docker build --build-arg num_procs=${NP} --build-arg version=${VERSION} -t $USERNAME/$IMAGE:$VERSION .
+docker build --build-arg num_procs=${NP} --build-arg version=${VERSION} --build-arg vdb_version=${VDB_VERSION} -t $USERNAME/$IMAGE:$VERSION .
 docker tag $USERNAME/$IMAGE:$VERSION $USERNAME/$IMAGE:latest
