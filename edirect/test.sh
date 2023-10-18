1326884e9c612ef5b099eea479a7e83081685d14
Christiam Camacho <christiam@hotmail.com>
2023-10-18 20:56:45 +0000

    Work in progress

--- edirect/test.sh
+++ edirect/test.sh
@@ -4,5 +4,8 @@
 # Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
 # Created: Fri 07 Dec 2018 03:45:23 PM EST
 IMG=${1:-"ncbi/edirect:latest"}
-docker run --rm -it ${IMG} installconfirm
+docker run --rm -it ${IMG} ./installconfirm
+#docker run --rm -it ${IMG} esearch -version
+#docker run --rm -it ${IMG} xtract -version
+#docker run --rm -it ${IMG} bash -c 'esearch -db pubmed -query "Babalobi OO[au] AND 2008[pdat]" | efetch -format xml | xtract -pattern Author -if Affiliation -contains Medicine -element Initials'
 docker run --rm -it ${IMG} efetch -db nucleotide -id u00001 -format fasta
