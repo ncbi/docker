ace66ba8191ecec9e57f1ff0d37d7d6779b8eb92
Christiam Camacho <christiam@users.noreply.github.com>
2023-10-20 13:20:02 -0400

    Update edirect 2023 10 (#38)
    * Work in progress
    
    * Work in progress
    
    * Remove commented out code

--- edirect/test.sh
+++ edirect/test.sh
@@ -4,5 +4,5 @@
 # Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
 # Created: Fri 07 Dec 2018 03:45:23 PM EST
 IMG=${1:-"ncbi/edirect:latest"}
-docker run --rm -it ${IMG} installconfirm
+docker run --rm -it ${IMG} ./installconfirm
 docker run --rm -it ${IMG} efetch -db nucleotide -id u00001 -format fasta
