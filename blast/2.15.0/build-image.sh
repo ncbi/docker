069faf8cdcfb6294bff6a2001b9baaab66574b49
Christiam Camacho <christiam@hotmail.com>
2023-11-01 19:17:36 +0000

    Upgrade VDB to 3.0.8

--- blast/2.15.0/build-image.sh
+++ blast/2.15.0/build-image.sh
@@ -6,7 +6,7 @@ VERSION=$(cat VERSION)
 # Check the latest ncbi-vdb release version in https://github.com/ncbi/ncbi-vdb/tags
 # To see which VDB version is currently used by C++ Toolkit do:
 #  grep local_vdb_base src/build-system/config.site.ncbi
-VDB_VERSION=3.0.1
+VDB_VERSION=3.0.8
 NP=$(grep -c ^proc /proc/cpuinfo)
 
 docker build --build-arg version="${VERSION}" --build-arg num_procs="${NP}" --build-arg vdb_version=${VDB_VERSION} -t "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" .
