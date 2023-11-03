0aa105915529fc96d87b558370f55b22f17fc7d9
Christiam Camacho <christiam@hotmail.com>
2023-11-03 23:11:20 +0000

    Enable follow symlinks in curl, capture docker build output

--- blast-static/build-image.sh
+++ blast-static/build-image.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -eux
+#!/bin/bash 
+set -xeuo pipefail
 
 DOCKERHUB_USERNAME=${1:-"ncbi"}
 IMAGE=blast-static
-VERSION=$(curl https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/VERSION)
+VERSION=$(curl -sSL https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/VERSION)
 
 case $(uname -m) in
 	x86_64) ARCH=amd64;;
@@ -10,6 +11,6 @@ case $(uname -m) in
 esac
 
 
-docker build --build-arg version="${VERSION}" -t "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" .
+docker build --progress=plain --build-arg version="${VERSION}" -t "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" --pull --no-cache . 2>&1 | tee build.log
 docker tag "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" "$DOCKERHUB_USERNAME"/$IMAGE:latest
 docker tag "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" "$DOCKERHUB_USERNAME"/$IMAGE:"${VERSION}"-${ARCH}
