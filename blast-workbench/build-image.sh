3b744e37ae30c42de0ea55d6972ff884afb5fb08
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2023-11-06 07:54:48 -0500

    Remove blast-workbench

--- blast-workbench/build-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-DOCKERHUB_USERNAME=${1:-"ncbi"}
-IMAGE=blast-workbench
-VERSION=0.4
-
-docker build -t $DOCKERHUB_USERNAME/$IMAGE:$VERSION .
-docker tag $DOCKERHUB_USERNAME/$IMAGE:$VERSION $DOCKERHUB_USERNAME/$IMAGE:latest
