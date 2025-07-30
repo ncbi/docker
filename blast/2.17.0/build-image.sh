3451aa3101678e43f6a6bf4d848e7200a0121428
Greg Boratyn <boratyng@ncbi.nlm.nih.gov>
2025-07-30 11:28:22 -0400

    Removed unnecessary code

--- blast/2.17.0/build-image.sh
+++ blast/2.17.0/build-image.sh
@@ -13,6 +13,4 @@ NP=$(grep -c ^proc /proc/cpuinfo)
 docker build --progress=plain --build-arg blast_version="${VERSION}" --build-arg num_procs="${NP}" --build-arg vdb_version=${VDB_VERSION} -t "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" --pull --no-cache . 2>&1 | tee build.log
 docker tag "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" "$DOCKERHUB_USERNAME"/$IMAGE:latest
 
-#docker history --format json --no-trunc -H "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" | jq -M . | tee docker-"$DOCKERHUB_USERNAME"-$IMAGE-"$VERSION".json
-docker history --format '{{json .}}' --no-trunc -H "$DOCKERHUB_USERNAME"/$IMAGE:"$VERSION" | jq -M . | tee docker-"$DOCKERHUB_USERNAME"-$IMAGE-"$VERSION".json
 docker image ls
